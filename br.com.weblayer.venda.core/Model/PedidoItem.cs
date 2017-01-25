795f5f0325a92256e0cc05acc592ecd039bd117c
DouglasCristhian (NoteBook DELL) <dc@weblayer.com.br>
2017-01-25 10:28:48 -0200

    --

--- br.com.weblayer.venda.core/Model/PedidoItem.cs
+++ br.com.weblayer.venda.core/Model/PedidoItem.cs
@@ -14,6 +14,9 @@ namespace br.com.weblayer.venda.core.Model
         [MaxLength(15), NotNull]
         public int id_produto { get; set; }
 
+        [MaxLength(200), NotNull]
+        public string ds_produto { get; set; }
+
         [MaxLength(20), NotNull]
         public double vl_item { get; set; }
 
